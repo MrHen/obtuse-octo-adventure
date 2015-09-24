7bd214b9d2594c40e984633681bbc2dd483df8d7
Adam Babcock <adam.babcock@smartsocialmedia.com>
2015-09-23 23:12:08 -0500

    Use appropriate port

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,13 @@ import express = require('express');
 import http = require('http');
 
 var app = express();
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('app'));
 
 var server = http.createServer(app);
 var server_config = { port: 4000 };
 
-server.listen(server_config.port, () => {
+server.listen(app.get('port'), () => {
     console.info('Express server listening', {port: server_config.port});
 });
